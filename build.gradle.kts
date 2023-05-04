d0e54b3891cad839423124434d850ef245248c0f
Debasish Bose <devbose2008@gmail.com>
2023-05-04 18:15:17 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val koinVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    id("io.ktor.plugin") version "2.3.0"
+                id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+}
+
+group = "com.dbose"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-server-config-yaml:$ktorVersion")
+    //koin for ktor
+    implementation ("io.insert-koin:koin-core:$koinVersion")
+    // SLF4J Logger
+    implementation ("io.insert-koin:koin-logger-slf4j:$koinVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+}
---
+++ src/main/kotlin/com/dbose/Application.kt
@@ -0,0 +1,14 @@
+package com.dbose
+
+import io.ktor.server.application.*
+import com.dbose.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
